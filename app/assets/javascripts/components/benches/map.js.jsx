cf67c1248d0b3470bace71a46019b0d5b830f8dc
Dan Cherouny <dcherouny@gmail.com>
2015-11-13 14:06:29 -0500

    Begin removing and adding of markers on scroll

--- app/assets/javascripts/components/benches/map.js.jsx
+++ app/assets/javascripts/components/benches/map.js.jsx
@@ -1,7 +1,7 @@
 var BenchesMap = React.createClass({
 
   getInitialState: function () {
-    return { map: 'undefined'};
+    return { map: 'undefined', markers: []};
   },
 
   componentDidMount: function () {
@@ -29,21 +29,50 @@ var BenchesMap = React.createClass({
     });
 
     this.setState({map: newMap});
-
-
   },
 
   onBenchIndexChange: function () {
 
-    var benches = BenchStore.all();
-    benches.forEach(function (bench) {
-      var LatLng = { lat: bench.lat, lng: bench.long };
+    var benches = BenchStore.all() || [];
+    var markers = this.state.markers.slice() || [];
+
+    addMarkers.call(this, benches, markers);
+    removeMarkers.call(this, benches, markers);
+
+
+    // if (benches) {
+    //   benches.forEach(function (bench) {
+    //     debugger;
+    //
+    //     var markerOverlap = markers.filter(function (marker) {
+    //
+    //       return(
+    //         benchLat === markerLat &&
+    //         benchLng === markerLng
+    //       );
+    //     });
+    //
+    //     if (markerOverlap.length > markers.length) {
+    //       markerOverlap.forEach(function (overLapMarker) {
+    //         overLapMarker.setMap(null);
+    //         markers.splice(markers.indexOf(overLapMarker), 1);
+    //       });
+    //     } else {
+    //     }
+      //
+      //   this.setState({ markers: markers });
+      // }.bind(this));
+    // }
+  },
+
+  makeMarker: function (bench) {
+    var LatLng = { lat: bench.lat, lng: bench.long };
+    return (
       new google.maps.Marker({
         position: LatLng,
-        map: this.state.map,
         title: bench.description
-      });
-    }.bind(this));
+      })
+    );
   },
 
   render: function () {
@@ -54,16 +83,62 @@ var BenchesMap = React.createClass({
   }
 });
 
-  var parseBounds = function () {
-    var bounds = this.getBounds();
+var parseBounds = function () {
+  var bounds = this.getBounds();
 
-    var northEastLat = bounds.getNorthEast().lat();
-    var northEastLng = bounds.getNorthEast().lng();
-    var southWestLat = bounds.getSouthWest().lat();
-    var southWestLng = bounds.getSouthWest().lng();
+  var northEastLat = bounds.getNorthEast().lat();
+  var northEastLng = bounds.getNorthEast().lng();
+  var southWestLat = bounds.getSouthWest().lat();
+  var southWestLng = bounds.getSouthWest().lng();
 
-    return {
-      northEast: { lat: northEastLat, lng: northEastLng },
-      southWest: { lat: southWestLat, lng: southWestLng }
-    };
+  return {
+    northEast: { lat: northEastLat, lng: northEastLng },
+    southWest: { lat: southWestLat, lng: southWestLng }
   };
+};
+
+var addMarkers = function (benches, markers) {
+  for (var i = 0; i < benches.length; i++) {
+    var matched = false;
+    var benchMarker = this.makeMarker(benches[i]);
+    var benchLat = benchMarker.getPosition().lat();
+    var benchLng = benchMarker.getPosition().lng();
+
+    for (var j = 0; j < markers.length; j++) {
+      var markerLat = markers[i].getPosition().lat();
+      var markerLng = markers[i].getPosition().lng();
+
+      if (benchLat === markerLat || benchLng === markerLng) {
+        matched = true;
+      }
+    }
+
+    if (!matched) {
+      benchMarker.setMap(this.state.map);
+      markers.push(benchMarker);
+    }
+  }
+};
+
+var removeMarkers = function (benches, markers) {
+  for (i = 0; i < markers.length; i++) {
+    var matched = false;
+    var markerLat = markers[i].getPosition().lat();
+    var markerLng = markers[i].getPosition().lng();
+
+    for (j = 0; j < benches.length; j++) {
+      var benchMarker = this.makeMarker(benches[i]);
+      var benchLat = benchMarker.getPosition().lat();
+      var benchLng = benchMarker.getPosition().lng();
+
+      if (benchLat === markerLat || benchLng === markerLng) {
+        matched = true;
+      }
+    }
+
+    if (!matched) {
+      benchMarker.setMap(null);
+      markers.splice(markers.indexOf(benchMarker));
+    }
+  }
+};
